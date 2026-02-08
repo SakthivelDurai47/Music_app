b578d1834b96b618b7c259093984f4e87b8df32a
SakthivelDurai47 <sakthiveldurai47@gmail.com>
2026-02-08 17:11:03 +0530

    updated new ui colors and layout

--- app.js
+++ app.js
@@ -107,11 +107,11 @@ function updateLikeList() {
     <img src='${songs[id].cover}' width="60"/>
     <p>${songs[id].title}</p>
     <button onclick="likeSong('${id}')">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ff003c">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#f6f6f6c4">
         <path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/>
       </svg>
     </button>
-    <button onclick="playSong('${id}')"><svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 -960 960 960" width="28px" fill="#e3e3e3"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg></button>
+    <button onclick="playSong('${id}')"><svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 -960 960 960" width="28px" fill="#f6f6f6c4"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg></button>
     `;
     likedList.appendChild(div);
     div.classList.add("songList");
@@ -119,7 +119,7 @@ function updateLikeList() {
   });
   if (likedList.innerHTML == "") {
     likedList.innerHTML =
-      "<h3>Nothing to see here, start liking music to add it here</h3>";
+      "<h4>Nothing to see here :(<br>start liking music to add it here</h4>";
   }
   updatePlayUI(likedSongs[song_count]);
 }
@@ -139,13 +139,13 @@ function updateAllList() {
     <p>${song.title}</p>
     
     <button onclick="likeSong('${id}')">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#f6f6f6c4">
         <path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/>
       </svg>
     </button>
 
     <button onclick="playSong('${id}')">
-      <svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 -960 960 960" width="28px" fill="#e3e3e3">
+      <svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 -960 960 960" width="28px" fill="#f6f6f6c4">
         <path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/>
       </svg>
     </button>
